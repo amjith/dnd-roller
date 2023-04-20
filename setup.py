c5687d2928189cd85b28cf34d98aea05d62b3ec5
Amjith Ramanujam <amjith.r@gmail.com>
2023-04-20 11:35:40 -0600

    change version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(PKG_FOLDER / "README.md") as f:
 
 setup(
     name="dnd-roller",
-    version="0.0.1",
+    version="0.0.2",
     author="Author",
     author_email="author@example.com",
     description="Python package to roll D&D dice in the terminal.",
